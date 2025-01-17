806fce3835e8485dbcbcdd87f3d772cfcccee533
Allan <allan.mendes0976@gmail.com>
2025-01-16 22:51:39 -0300

    feat: create exercise

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o número da sua conta: ");
+        int numero = scanner.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Digite o número da sua Agência: ");
+        String agencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.print("Digite o seu nome completo: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Deposite um valor mínimo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
